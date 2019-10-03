59d5500c202ae41d94c3fcc2ddcda466348a26ca
Chris Kerbo <chris.kerbo@outlook.com>
2019-10-03 16:35:06 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,12 @@
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :status, :name
+
+  def initialize(name)
+    @name = name
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+  end
+
 end
