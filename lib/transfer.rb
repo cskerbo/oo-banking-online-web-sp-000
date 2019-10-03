0403a664dbcb2eedac386e867d5b21af2137d567
Chris Kerbo <chris.kerbo@outlook.com>
2019-10-03 17:23:38 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,4 +13,18 @@ class Transfer
     sender.valid? && receiver.valid?
   end
 
+  def execute_transaction
+    if @sender.balance < @amount
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    elsif @status == "complete"
+      return "Transfer already executed."
+    else
+      @sender.deposit(@amount * -1)
+      @receiver.deposit(@amount)
+      @status = "complete"
+    end
+  end
+
+
 end
