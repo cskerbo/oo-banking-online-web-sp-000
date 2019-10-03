209563f7996237bece112c379064210280b62967
Chris Kerbo <chris.kerbo@outlook.com>
2019-10-03 17:06:22 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,7 +10,7 @@ class Transfer
   end
 
   def valid?
-    (sender.status == "open" && sender.balance > 0) && (receiver.status == "open" && receiver.balance > 0)
+    self.status == "open" && self.balance > 0
   end
 
 end
