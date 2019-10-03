5973ee18bd093828f4e5cb3a45310ee3f40aac95
Chris Kerbo <chris.kerbo@outlook.com>
2019-10-03 17:31:33 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance < @amount
+    if @sender.balance < self.amount
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     elsif @status == "complete"
